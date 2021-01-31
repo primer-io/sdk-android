e97a491ee1f5e072d9a044a79e6abfb44103cd66
StickyCube <will.jk01@gmail.com>
2021-01-31 14:41:38 +0100

    Add button placement

--- primer-sdk-android/src/main/java/io/primer/android/ui/fragments/FormControlsFragment.kt
+++ primer-sdk-android/src/main/java/io/primer/android/ui/fragments/FormControlsFragment.kt
@@ -1,17 +1,22 @@
 package io.primer.android.ui.fragments
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import io.primer.android.R
 import io.primer.android.logging.Logger
 import io.primer.android.ui.ButtonState
 
 internal class FormControlsFragment : FormChildFragment() {
   private val log = Logger("form-controls")
+  private lateinit var layout: ViewGroup
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -24,14 +29,25 @@ internal class FormControlsFragment : FormChildFragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
+    layout = view.findViewById(R.id.form_button)
+
     viewModel.button.observe(viewLifecycleOwner) {
       it?.let { state ->
-        val buttonText = view.findViewById<TextView>(R.id.form_button_txt)
-        val buttonLoading = view.findViewById<ProgressBar>(R.id.form_button_loading)
+        val buttonText = layout.findViewById<TextView>(R.id.form_button_txt)
+        val buttonLoading = layout.findViewById<ProgressBar>(R.id.form_button_loading)
+
+        layout.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT).apply {
+          gravity = when(state.placement) {
+            ButtonState.Placement.LEFT -> Gravity.START
+            ButtonState.Placement.RIGHT -> Gravity.END
+            else -> Gravity.CENTER_HORIZONTAL
+          }
+        }
 
         buttonText.text = requireContext().getString(state.labelId)
+
         buttonLoading.visibility = if (state.loading) View.VISIBLE else View.GONE
-        view.isEnabled = isViewEnabled(viewModel.isValid.value ?: true, it)
+        layout.isEnabled = isViewEnabled(viewModel.isValid.value ?: true, it)
       }
     }
 
