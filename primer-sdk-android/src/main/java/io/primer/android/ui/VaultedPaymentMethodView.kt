2681564e33cb60c2ecac2f20fc07885e19251e9c
takecare <rui@vazteixeira.org>
2021-03-23 17:24:16 +0000

    drop hungarian notation

--- primer-sdk-android/src/main/java/io/primer/android/ui/VaultedPaymentMethodView.kt
+++ primer-sdk-android/src/main/java/io/primer/android/ui/VaultedPaymentMethodView.kt
@@ -8,54 +8,52 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import io.primer.android.R
-import io.primer.android.logging.Logger
 import io.primer.android.payment.TokenAttributes
 
+// FIXME this should, itself, be a View
 internal class VaultedPaymentMethodView(context: Context, private val attributes: TokenAttributes) {
 
-    private val log = Logger("vaulted-payment-method")
-
-    private val mView: ViewGroup =
+    private val view: ViewGroup =
         View.inflate(context, R.layout.vaulted_payment_method_card, null) as ViewGroup
-    private val mListItemNormal =
+    private val listItemNormal =
         ResourcesCompat.getDrawable(context.resources, R.drawable.list_item_normal, null)
-    private val mListItemEditable =
+    private val listItemEditable =
         ResourcesCompat.getDrawable(context.resources, R.drawable.list_item_editable, null)
 
-    private val mDelete: Button
-        get() = mView.findViewById(R.id.item_delete_button)
+    private val deleteButton: Button
+        get() = view.findViewById(R.id.item_delete_button)
 
-    private val mIcon: ImageView
-        get() = mView.findViewById(R.id.item_icon_left)
+    private val iconImageView: ImageView
+        get() = view.findViewById(R.id.item_icon_left)
 
-    private val mContentLayout: ViewGroup
-        get() = mView.findViewById(R.id.item_content_layout)
+    private val contentLayout: ViewGroup
+        get() = view.findViewById(R.id.item_content_layout)
 
-    private val mDescription: TextView
-        get() = mView.findViewById(R.id.item_description)
+    private val descriptionTextView: TextView
+        get() = view.findViewById(R.id.item_description)
 
     init {
-        mIcon.setImageDrawable(
-            ResourcesCompat.getDrawable(mView.context.resources, attributes.icon, null)
+        iconImageView.setImageDrawable(
+            ResourcesCompat.getDrawable(view.context.resources, attributes.icon, null)
         )
-        mIcon.scaleType = ImageView.ScaleType.CENTER_CROP
-        mIcon.scaleX = attributes.iconScale
-        mIcon.scaleY = attributes.iconScale
-        mDescription.text = attributes.getDescription(context)
+        iconImageView.scaleType = ImageView.ScaleType.CENTER_CROP
+        iconImageView.scaleX = attributes.iconScale
+        iconImageView.scaleY = attributes.iconScale
+        descriptionTextView.text = attributes.getDescription(context)
         setEditable(false)
     }
 
     fun setOnDeleteListener(l: ((v: View) -> Unit)) {
-        mDelete.setOnClickListener(l)
+        deleteButton.setOnClickListener(l)
     }
 
     fun setEditable(isEditable: Boolean) {
-        mDelete.visibility = if (isEditable) View.VISIBLE else View.INVISIBLE
-        mContentLayout.background = if (isEditable) mListItemEditable else mListItemNormal
-        mContentLayout.elevation = if (isEditable) 12f else 0f
+        deleteButton.visibility = if (isEditable) View.VISIBLE else View.INVISIBLE
+        contentLayout.background = if (isEditable) listItemEditable else listItemNormal
+        contentLayout.elevation = if (isEditable) 12f else 0f
     }
 
     fun getView(): View {
-        return mView
+        return view
     }
 }
