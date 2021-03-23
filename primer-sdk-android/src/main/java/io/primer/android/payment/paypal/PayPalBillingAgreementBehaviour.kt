7651a5412eea6daf7769a4eaaf60f91d1efacc28
takecare <rui@vazteixeira.org>
2021-03-23 17:37:01 +0000

    remove redundant code

--- primer-sdk-android/src/main/java/io/primer/android/payment/paypal/PayPalBillingAgreementBehaviour.kt
+++ primer-sdk-android/src/main/java/io/primer/android/payment/paypal/PayPalBillingAgreementBehaviour.kt
@@ -20,22 +20,9 @@ internal class PayPalBillingAgreementBehaviour constructor(
         tokenizationViewModel?.resetPaymentMethod(paypal)
     }
 
-    override fun getUri(
-        cancelUrl: String,
-        returnUrl: String,
-//        callback: ((String) -> Unit)
-    ) {
+    override fun getUri(cancelUrl: String, returnUrl: String) {
         paypal.config.id?.let { id ->
-
             tokenizationViewModel?.createPayPalBillingAgreement(id, returnUrl, cancelUrl)
-
-//            tokenizationViewModel?.createPayPalBillingAgreement(id, returnUrl, cancelUrl)?.observe { e ->
-//                when (e) {
-//                    is Observable.ObservableSuccessEvent -> {
-//                        callback(e.data.getString("approvalUrl"))
-//                    }
-//                }
-//            }
         }
     }
 
@@ -43,30 +30,8 @@ internal class PayPalBillingAgreementBehaviour constructor(
         uri.getQueryParameter("ba_token")?.let { token ->
             paypal.config.id?.let { id ->
                 tokenizationViewModel?.confirmPayPalBillingAgreement(id, token)
-
-//                tokenizationViewModel?.confirmPayPalBillingAgreement(id, token)?.observe { e ->
-//                    when (e) {
-//                        is Observable.ObservableSuccessEvent -> tokenize(e.data)
-//                    }
-//                }
             }
         }
-    }
-
-    private fun tokenize(data: JSONObject) {
-        // we're relying on 'paypal' being passed by reference, this isn't good (flow isn't clear; other things can change it)
-        paypal.setTokenizableValue("paypalBillingAgreementId", data.getString("billingAgreementId"))
-        paypal.setTokenizableValue("externalPayerInfo", data.getJSONObject("externalPayerInfo"))
-        paypal.setTokenizableValue("shippingAddress", data.getJSONObject("shippingAddress"))
-
-        tokenizationViewModel?.tokenize()
-//        tokenizationViewModel?.tokenize()?.observe {
-//            when (it) {
-//                is Observable.ObservableSuccessEvent -> {
-//                    viewModel.viewStatus.value = ViewStatus.VIEW_VAULTED_PAYMENT_METHODS
-//                }
-//            }
-//        }
     }
 
     override fun onCancel(uri: Uri?) {
