4da82201757d6a4fd402bb154ee82bfa1f04042d
takecare <rui@vazteixeira.org>
2021-04-14 17:52:52 +0100

    add gpay payment method

--- primer-sdk-android/src/main/java/io/primer/android/PaymentMethod.kt
+++ primer-sdk-android/src/main/java/io/primer/android/PaymentMethod.kt
@@ -2,6 +2,7 @@ package io.primer.android
 
 import io.primer.android.model.OrderItem
 import io.primer.android.payment.GOCARDLESS_IDENTIFIER
+import io.primer.android.payment.GOOGLE_PAY_IDENTIFIER
 import io.primer.android.payment.KLARNA_IDENTIFIER
 import io.primer.android.payment.PAYMENT_CARD_IDENTIFIER
 import io.primer.android.payment.PAYPAL_IDENTIFIER
@@ -18,8 +19,11 @@ sealed class PaymentMethod(val identifier: String) {
     class PayPal : PaymentMethod(PAYPAL_IDENTIFIER)
 
     @Serializable
+    class GooglePay : PaymentMethod(GOOGLE_PAY_IDENTIFIER)
+
+    @Serializable
     class Klarna(
-        val orderItems: List<OrderItem>
+        val orderItems: List<OrderItem>,
     ) : PaymentMethod(KLARNA_IDENTIFIER)
 
     @Serializable
@@ -36,3 +40,28 @@ sealed class PaymentMethod(val identifier: String) {
         val customerAddressPostalCode: String? = null,
     ) : PaymentMethod(GOCARDLESS_IDENTIFIER)
 }
+
+interface _PaymentMethod
+
+@Serializable
+object _Card : _PaymentMethod
+
+@Serializable
+object _PayPal : _PaymentMethod
+
+@Serializable
+data class _Klarna(val orderItems: List<OrderItem>) : _PaymentMethod
+
+@Serializable
+data class _GoCardless(
+    val companyName: String,
+    val companyAddress: String,
+    val customerName: String? = null,
+    val customerEmail: String? = null,
+    val customerAddressLine1: String? = null,
+    val customerAddressLine2: String? = null,
+    val customerAddressCity: String? = null,
+    val customerAddressState: String? = null,
+    val customerAddressCountryCode: String? = null,
+    val customerAddressPostalCode: String? = null,
+) : _PaymentMethod
