464c5b4735f5f06c0a6c350762f7085ada189194
takecare <rui@vazteixeira.org>
2021-04-19 10:17:26 +0100

    remove unnecessary param from isReadyToPay request

--- primer-sdk-android/src/main/java/io/primer/android/payment/google/GooglePayBridge.kt
+++ primer-sdk-android/src/main/java/io/primer/android/payment/google/GooglePayBridge.kt
@@ -3,57 +3,48 @@ package io.primer.android.payment.google
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.wallet.IsReadyToPayRequest
 import com.google.android.gms.wallet.PaymentsClient
-import com.google.android.gms.wallet.Wallet
-import com.google.android.gms.wallet.WalletConstants
 import org.json.JSONArray
 import org.json.JSONObject
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
-
-//val walletOptions = Wallet.WalletOptions.Builder()
-//    .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
-//    .build()
-//val paymentsClient: PaymentsClient = Wallet.getPaymentsClient(this, walletOptions)
 
 // TODO rename to facade
 class GooglePayBridge(
     private val paymentsClient: PaymentsClient,
 ) {
 
-    // allowedCardNetworks = listOf("AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA")
-    // allowedCardAuthMethods = listOf("PAN_ONLY", "CRYPTOGRAM_3DS")
-    fun buildIsReadyToGooglePayRequest(
+    fun buildPaymentRequest(
         gatewayMerchantId: String,
+    ) {
+//        val gatewayParams = JSONObject(
+//            mapOf(
+//                "gateway" to "primer",
+//                "gatewayMerchantId" to gatewayMerchantId
+//            )
+//        )
+//        val gatewayTokenizationSpecification = JSONObject().apply {
+//            put("type", "PAYMENT_GATEWAY")
+//            put("parameters", gatewayParams)
+//        }
+
+//            put("tokenizationSpecification", gatewayTokenizationSpecification)
+    }
+
+    fun buildIsReadyToGooglePayRequest(
         allowedCardNetworks: List<String>,
         allowedCardAuthMethods: List<String>,
         billingAddressRequired: Boolean,
     ): JSONObject {
-        val gatewayParams = JSONObject(
-            mapOf(
-                "gateway" to "primer",
-                "gatewayMerchantId" to gatewayMerchantId
-            )
-        )
-        val gatewayTokenizationSpecification = JSONObject().apply {
-            put("type", "PAYMENT_GATEWAY")
-            put("parameters", gatewayParams)
-        }
-
         val baseCardPaymentMethods = JSONObject().apply {
             val parameters = JSONObject().apply {
                 put("allowedAuthMethods", JSONArray(allowedCardAuthMethods))
                 put("allowedCardNetworks", JSONArray(allowedCardNetworks))
                 put("billingAddressRequired", billingAddressRequired)
-                put("billingAddressParameters", JSONObject().apply {
-                    put("format", "FULL")
-                })
             }
 
             put("type", "CARD")
             put("parameters", parameters)
-            put("tokenizationSpecification", gatewayTokenizationSpecification)
         }
 
         return JSONObject().apply {
@@ -63,7 +54,20 @@ class GooglePayBridge(
         }
     }
 
-    suspend fun checkIfIsReadyToPay(request: JSONObject): Boolean =
+    suspend fun checkIfIsReadyToPay(
+        gatewayMerchantId: String,
+        allowedCardNetworks: List<String>,
+        allowedCardAuthMethods: List<String>,
+        billingAddressRequired: Boolean,
+    ): Boolean = checkIfIsReadyToPay(
+        buildIsReadyToGooglePayRequest(
+            allowedCardNetworks = allowedCardNetworks,
+            allowedCardAuthMethods = allowedCardAuthMethods,
+            billingAddressRequired = billingAddressRequired
+        )
+    )
+
+    private suspend fun checkIfIsReadyToPay(request: JSONObject): Boolean =
         suspendCoroutine { continuation: Continuation<Boolean> ->
             val isReadyToPayRequest = IsReadyToPayRequest.fromJson(request.toString())
             paymentsClient
@@ -74,7 +78,9 @@ class GooglePayBridge(
                             continuation.resume(isAvailable)
                         }
                     } catch (exception: ApiException) {
-                        continuation.resumeWithException(exception)
+                        // continuation.resumeWithException(exception)
+                        // TODO log error
+                        continuation.resume(false)
                     }
                 }
         }
