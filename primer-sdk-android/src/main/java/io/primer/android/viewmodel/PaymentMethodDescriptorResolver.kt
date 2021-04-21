51b76acc57f32ce444d57d6727b931028659124f
takecare <rui@vazteixeira.org>
2021-04-21 15:18:35 +0100

    extract function for readability

--- primer-sdk-android/src/main/java/io/primer/android/viewmodel/PaymentMethodDescriptorResolver.kt
+++ primer-sdk-android/src/main/java/io/primer/android/viewmodel/PaymentMethodDescriptorResolver.kt
@@ -3,60 +3,8 @@ package io.primer.android.viewmodel
 import io.primer.android.PaymentMethod
 import io.primer.android.model.dto.CheckoutConfig
 import io.primer.android.model.dto.ClientSession
-import io.primer.android.model.dto.PaymentMethodRemoteConfig
 import io.primer.android.payment.PaymentMethodDescriptor
 import io.primer.android.payment.PaymentMethodDescriptorFactory
-import io.primer.android.payment.google.GooglePayBridge
-import org.koin.core.component.KoinApiExtension
-
-internal interface PaymentMethodChecker {
-
-    suspend fun shouldPaymentMethodBeAvailable(
-        paymentMethod: PaymentMethod,
-        clientSession: ClientSession,
-    ): Boolean
-}
-
-internal class GooglePayPaymentMethodChecker constructor(
-    private val googlePayBridge: GooglePayBridge,
-) : PaymentMethodChecker {
-
-    override suspend fun shouldPaymentMethodBeAvailable(
-        paymentMethod: PaymentMethod,
-        clientSession: ClientSession,
-    ): Boolean {
-        val googlePay = paymentMethod as PaymentMethod.GooglePay
-        return googlePayBridge.checkIfIsReadyToPay(
-            allowedCardNetworks = googlePay.allowedCardNetworks,
-            allowedCardAuthMethods = googlePay.allowedCardAuthMethods,
-            billingAddressRequired = googlePay.billingAddressRequired
-        )
-    }
-}
-
-internal interface PaymentMethodCheckerRegistrar {
-
-    val checkers: Map<String, PaymentMethodChecker>
-
-    fun register(id: String, checker: PaymentMethodChecker)
-    fun unregister(id: String)
-
-    operator fun get(id: String): PaymentMethodChecker? = checkers[id]
-}
-
-internal object PrimerPaymentMethodCheckerRegistrar : PaymentMethodCheckerRegistrar {
-
-    private val _checkers: MutableMap<String, PaymentMethodChecker> = mutableMapOf()
-    override val checkers: Map<String, PaymentMethodChecker> = _checkers
-
-    override fun register(id: String, checker: PaymentMethodChecker) {
-        _checkers[id] = checker
-    }
-
-    override fun unregister(id: String) {
-        _checkers.remove(id)
-    }
-}
 
 internal interface PaymentMethodDescriptorResolver {
 
@@ -70,7 +18,6 @@ internal class PrimerPaymentMethodDescriptorResolver constructor(
     private val availabilityCheckers: PaymentMethodCheckerRegistrar,
 ) : PaymentMethodDescriptorResolver {
 
-    //    override suspend fun resolve(remotePaymentMethods: List<PaymentMethodRemoteConfig>): List<PaymentMethodDescriptor> {
     override suspend fun resolve(clientSession: ClientSession): List<PaymentMethodDescriptor> {
         val remotePaymentMethods = clientSession.paymentMethods
         val list = ArrayList<PaymentMethodDescriptor>()
@@ -78,13 +25,7 @@ internal class PrimerPaymentMethodDescriptorResolver constructor(
         remotePaymentMethods.forEach { paymentMethodRemoteConfig ->
             localPaymentMethods
                 .find { it.identifier == paymentMethodRemoteConfig.type }
-                ?.takeUnless { paymentMethod: PaymentMethod ->
-                    val checker = availabilityCheckers[paymentMethod.identifier]
-                    checker != null && !checker.shouldPaymentMethodBeAvailable(
-                        paymentMethod = paymentMethod,
-                        clientSession = clientSession,
-                    )
-                }
+                ?.takeUnlessUnavailable(clientSession)
                 ?.let {
                     paymentMethodDescriptorFactory
                         .create(
@@ -92,10 +33,19 @@ internal class PrimerPaymentMethodDescriptorResolver constructor(
                             paymentMethodRemoteConfig = paymentMethodRemoteConfig,
                             paymentMethod = it,
                         )
-                        ?.let { paymentMethodDescriptor -> list.add(paymentMethodDescriptor) }
                 }
+                ?.let { list.add(it) }
         }
 
         return list
     }
+
+    private suspend fun PaymentMethod.takeUnlessUnavailable(clientSession: ClientSession) =
+        takeUnless {
+            val checker = availabilityCheckers[identifier]
+            checker != null && !checker.shouldPaymentMethodBeAvailable(
+                paymentMethod = this,
+                clientSession = clientSession,
+            )
+        }
 }
