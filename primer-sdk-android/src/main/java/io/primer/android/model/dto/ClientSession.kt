c3d303337e7f0b33698b85e0c5fd9647e7bdcee0
takecare <rui@vazteixeira.org>
2021-04-28 15:15:06 +0100

    add env field to ClientSession

--- primer-sdk-android/src/main/java/io/primer/android/model/dto/ClientSession.kt
+++ primer-sdk-android/src/main/java/io/primer/android/model/dto/ClientSession.kt
@@ -1,5 +1,6 @@
 package io.primer.android.model.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,4 +8,12 @@ data class ClientSession(
     val pciUrl: String,
     val coreUrl: String,
     val paymentMethods: List<PaymentMethodRemoteConfig>,
+    @SerialName("env") val environment: Environment,
 )
+
+enum class Environment(val environment: String) {
+    DEV("dev"),
+    SANDBOX("sandbox"),
+    STAGING("staging"),
+    PRODUCTION("production"),
+}
