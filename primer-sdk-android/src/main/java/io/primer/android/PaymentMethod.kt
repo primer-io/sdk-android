be91d8209c1ebb47c32453da1feee4580706be1a
takecare <rui@vazteixeira.org>
2021-04-29 17:37:42 +0100

    add docs

--- primer-sdk-android/src/main/java/io/primer/android/PaymentMethod.kt
+++ primer-sdk-android/src/main/java/io/primer/android/PaymentMethod.kt
@@ -1,13 +1,34 @@
 package io.primer.android
 
+import io.primer.android.payment.PaymentMethodDescriptor
+import io.primer.android.payment.PaymentMethodDescriptorFactory
+import io.primer.android.viewmodel.PaymentMethodChecker
 import kotlinx.serialization.Transient
 import kotlinx.serialization.modules.SerializersModule
 
-// this new way of declaring PaymentMethods is meant to improve modularisation as each method can
-// be declared in a separate module (provided the marker interface is declared in shared one)
+/**
+ * Represents an abstract **PaymentMethod**. A PaymentMethod declares a [PaymentMethodModule] and a
+ * [SerializersModule].
+ * A [PaymentMethodModule] is used to:
+ * 1. Initialize any internal dependencies it may have (see [PaymentMethodModule.initialize]);
+ * 1. Declare its [PaymentMethodChecker] it may require, which will determine if it (the
+ * PaymentMethod) will be available at run-time, or not;
+ * 1. Declare its [PaymentMethodDescriptorFactory] that will be used to create the corresponding
+ * [PaymentMethodDescriptor].
+ *
+ * The [SerializersModule] is necessary so that Kotlin's serialization is properly setup, in order
+ * for all payment methods to be serialized and passed from the [UniversalCheckout] to the
+ * [CheckoutSheetActivity]. Note that this is currently necessary but ideally we'll be removing it
+ * (e.g. if we stop requiring the host app to declare all payment methods it wants to support
+ * via the [UniversalCheckout]).
+ * @see [PaymentMethodModule]
+ * @see [SerializersModule]
+ */
 interface PaymentMethod {
 
-    // this is here for backwards compatibility with old_PaymentMethod (to avoid a larger refactor)
+    // FIXME this is here for backwards compatibility as we're still relying on identifiers
+    //  ideally all payment methods would be parsed into the respective classes (that implement
+    //  this interface) at runtime
     val identifier: String
 
     @Transient
--- primer-sdk-android/src/main/java/io/primer/android/PaymentMethodModule.kt
+++ primer-sdk-android/src/main/java/io/primer/android/PaymentMethodModule.kt
@@ -2,20 +2,53 @@ package io.primer.android
 
 import android.content.Context
 import io.primer.android.model.dto.ClientSession
+import io.primer.android.payment.PaymentMethodDescriptor
+import io.primer.android.payment.PaymentMethodDescriptorFactory
 import io.primer.android.payment.PaymentMethodDescriptorFactoryRegistry
+import io.primer.android.viewmodel.PaymentMethodChecker
 import io.primer.android.viewmodel.PaymentMethodCheckerRegistry
 
+/**
+ * A **PaymentMethodModule** contains all the dependencies of a particular [PaymentMethod]. It can
+ * be [initialize]d if necessary and it can also register a [PaymentMethodChecker] with the
+ * [PaymentMethodCheckerRegistry], as well as a [PaymentMethodDescriptorFactory] with the
+ * [PaymentMethodDescriptorFactoryRegistry].
+ */
 interface PaymentMethodModule {
 
+    /**
+     * Initializes this payment method module, passing in all the information it may need to do
+     * initialize itself.
+     */
     fun initialize(
         applicationContext: Context,
-        clientSession: ClientSession
+        clientSession: ClientSession,
     )
 
+    /**
+     * To be called when the SDK is at its startup phase. Each [PaymentMethod] can declare its
+     * [PaymentMethodModule] specifying (among other things) its [PaymentMethodChecker], that will
+     * be run at an appropriate time in order to determine if it (the [PaymentMethod]) should be
+     * made available or not, at run-time.
+     * @see [PaymentMethod]
+     * @see [PaymentMethodDescriptor]
+     * @see [PaymentMethodCheckerRegistry]
+     * @see [PrimerPaymentMethodDescriptorResolver]
+     */
     fun registerPaymentMethodCheckers(
         paymentMethodCheckerRegistry: PaymentMethodCheckerRegistry,
     )
 
+    /**
+     * To be called when the SDK is at its startup phase. Each [PaymentMethod] can declare its
+     * [PaymentMethodModule] specifying (among other things) its [PaymentMethodDescriptorFactory].
+     * This [PaymentMethodDescriptorFactory] will be used to create a [PaymentMethodDescriptor].
+     * when needed.
+     * @see [PaymentMethod]
+     * @see [PaymentMethodDescriptor]
+     * @see [PaymentMethodDescriptorFactoryRegistry]
+     * @see [PrimerPaymentMethodDescriptorResolver]
+     */
     fun registerPaymentMethodDescriptorFactory(
         paymentMethodDescriptorFactoryRegistry: PaymentMethodDescriptorFactoryRegistry,
     )
