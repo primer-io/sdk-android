fe746c63b8262fa78705fd24c47ff9b1bd652363
takecare <rui@vazteixeira.org>
2021-05-07 14:31:27 +0100

    add debug build type

---
+++ primer-sdk-android/src/debug/java/io/primer/android/DebugActivity.kt
@@ -0,0 +1,24 @@
+package io.primer.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class DebugActivity : AppCompatActivity() {
+
+    private lateinit var motionLayout: MotionLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debug)
+
+        motionLayout = findViewById(R.id.motionLayout)
+        findViewById<ConstraintLayout>(R.id.saved_payment_method).setOnClickListener {
+            when (motionLayout.currentState) {
+                motionLayout.startState -> motionLayout.transitionToEnd()
+                else -> motionLayout.transitionToStart()
+            }
+        }
+    }
+}
