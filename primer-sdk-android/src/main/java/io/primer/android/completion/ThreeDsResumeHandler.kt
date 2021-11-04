dc9af7bfe81e36c82ecc4754f135d0bf7fa75141
Semir <semir@primer.io>
2021-11-04 14:01:12 +0100

    Check if 3DS is included when resuming payments

--- primer-sdk-android/src/main/java/io/primer/android/completion/ThreeDsResumeHandler.kt
+++ primer-sdk-android/src/main/java/io/primer/android/completion/ThreeDsResumeHandler.kt
@@ -4,19 +4,28 @@ import io.primer.android.domain.token.repository.ClientTokenRepository
 import io.primer.android.events.CheckoutEvent
 import io.primer.android.events.EventDispatcher
 import io.primer.android.logging.Logger
+import io.primer.android.model.dto.APIError
 import io.primer.android.threeds.domain.respository.PaymentMethodRepository
+import io.primer.android.threeds.helpers.ThreeDsSdkClassValidator
 
 internal class ThreeDsResumeHandler(
     clientTokenRepository: ClientTokenRepository,
     paymentMethodRepository: PaymentMethodRepository,
+    private val threeDsSdkClassValidator: ThreeDsSdkClassValidator,
     private val eventDispatcher: EventDispatcher,
     logger: Logger
 ) : DefaultResumeHandler(clientTokenRepository, paymentMethodRepository, eventDispatcher, logger) {
 
     override fun handleClientToken(clientToken: String) {
         super.handleClientToken(clientToken)
-        eventDispatcher.dispatchEvent(
-            CheckoutEvent.Start3DS
+        if (threeDsSdkClassValidator.is3dsSdkIncluded()) {
+            eventDispatcher.dispatchEvent(
+                CheckoutEvent.Start3DS
+            )
+        } else eventDispatcher.dispatchEvent(
+            CheckoutEvent.ResumeError(
+                APIError(ThreeDsSdkClassValidator.THREE_DS_CLASS_NOT_LOADED_ERROR)
+            )
         )
     }
 }
