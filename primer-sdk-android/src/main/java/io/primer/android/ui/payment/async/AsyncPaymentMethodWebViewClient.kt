5f4f121f2fa0fcce18ebd459adf7c555316fe7e5
Semir <semir@primer.io>
2023-03-09 23:03:17 +0100

    [CHKT-1095] Implemented handling of cancellation urls for APMs

--- primer-sdk-android/src/main/java/io/primer/android/ui/payment/async/AsyncPaymentMethodWebViewClient.kt
+++ primer-sdk-android/src/main/java/io/primer/android/ui/payment/async/AsyncPaymentMethodWebViewClient.kt
@@ -7,17 +7,27 @@ import io.primer.android.ui.base.webview.BaseWebViewClient
 import io.primer.android.ui.base.webview.WebViewActivity
 
 internal class AsyncPaymentMethodWebViewClient(
-    activity: WebViewActivity,
+    private val activity: WebViewActivity,
     url: String?,
     returnUrl: String?,
 ) : BaseWebViewClient(activity, url, returnUrl) {
 
-    override fun getUrlState(url: String) = UrlState.PROCESSING
+    override fun getUrlState(url: String) = when {
+        Uri.parse(url).pathSegments.contains(CANCEL_STATE_QUERY_PARAM) -> UrlState.CANCELLED
+        else -> UrlState.PROCESSING
+    }
 
     override fun getCaptureUrl(url: String?) = url
 
     override fun canCaptureUrl(url: String?) = CAPTURE_URLS.any { url?.contains(it) == true } ||
         super.canCaptureUrl(url)
+
+    override fun onUrlCaptured(intent: Intent) {
+        when (getUrlState(intent.data.toString())) {
+            UrlState.CANCELLED -> activity.onBackPressed()
+            else -> super.onUrlCaptured(intent)
+        }
+    }
 
     override fun handleDeepLink(uri: Uri?): Boolean {
         getIntentFromUri(uri)?.let {
@@ -31,8 +41,10 @@ internal class AsyncPaymentMethodWebViewClient(
         Log.e(TAG, "Cannot handle intent: ${intent.data}")
     }
 
-    private companion object {
-        val CAPTURE_URLS =
+    internal companion object {
+
+        private val CAPTURE_URLS =
             listOf("primer.io/static/loading.html", "primer.io/static/loading-spinner.html")
+        const val CANCEL_STATE_QUERY_PARAM = "cancel"
     }
 }
