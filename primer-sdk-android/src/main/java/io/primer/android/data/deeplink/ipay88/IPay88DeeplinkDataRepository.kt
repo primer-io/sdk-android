261f3b4bdf3585fc80b0bdc425c8099f33f821d4
HarryPrimer <135856159+HarryPrimer@users.noreply.github.com>
2023-07-19 17:28:22 +0100

    CHKT-1422 Update IPay88 SDK (#407)
    * CHKT-1422: Updating primerIPay88 SDK version
    
    * CHKT-1422: Updating IPay88 deeplink schema
    
    * CHKT-1422: Updating IPay88 sdk version in the example app

--- primer-sdk-android/src/main/java/io/primer/android/data/deeplink/ipay88/IPay88DeeplinkDataRepository.kt
+++ primer-sdk-android/src/main/java/io/primer/android/data/deeplink/ipay88/IPay88DeeplinkDataRepository.kt
@@ -9,12 +9,14 @@ internal class IPay88DeeplinkDataRepository(private val metaDataSource: MetaData
     override fun getDeeplinkUrl() =
         Uri.Builder().scheme(PRIMER_REDIRECT_SCHEMA)
             .authority("$PRIMER_REDIRECT_PREFIX${metaDataSource.getApplicationId()}")
+            .appendPath(PRIMER_REDIRECT_PATH_PREFIX)
             .build()
             .toString()
 
     private companion object {
 
-        const val PRIMER_REDIRECT_SCHEMA = "primer_ipay88"
+        const val PRIMER_REDIRECT_SCHEMA = "primer"
         const val PRIMER_REDIRECT_PREFIX = "requestor."
+        const val PRIMER_REDIRECT_PATH_PREFIX = "ipay88"
     }
 }
