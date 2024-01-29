02303910d0897d89f6263f038ac7f3e1c8afe58e
semirp <85510694+semirp@users.noreply.github.com>
2024-01-29 12:12:55 +0100

    fix: Fixed wrong platform sent when using Web redirect payment methods (#504)

--- primer-sdk-android/src/main/java/io/primer/android/data/tokenization/models/paymentInstruments/async/webRedirect/WebRedirectSessionInfoDataRequest.kt
+++ primer-sdk-android/src/main/java/io/primer/android/data/tokenization/models/paymentInstruments/async/webRedirect/WebRedirectSessionInfoDataRequest.kt
@@ -7,7 +7,7 @@ import org.json.JSONObject
 internal data class WebRedirectSessionInfoDataRequest(
     override val locale: String,
     val redirectionUrl: String
-) : BaseSessionInfoDataRequest(locale, redirectionUrl) {
+) : BaseSessionInfoDataRequest(locale) {
 
     companion object {
         @JvmField
