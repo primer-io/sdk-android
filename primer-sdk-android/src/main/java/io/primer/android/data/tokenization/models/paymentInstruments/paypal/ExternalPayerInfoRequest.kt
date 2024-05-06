09357d22ba41362d23c55f8d5788f90cfae1ffc3
semirp <85510694+semirp@users.noreply.github.com>
2024-05-06 17:17:47 +0200

    fix: Added missing paypal checkout tokenization params - first and last name (#565)

--- primer-sdk-android/src/main/java/io/primer/android/data/tokenization/models/paymentInstruments/paypal/ExternalPayerInfoRequest.kt
+++ primer-sdk-android/src/main/java/io/primer/android/data/tokenization/models/paymentInstruments/paypal/ExternalPayerInfoRequest.kt
@@ -6,18 +6,24 @@ import org.json.JSONObject
 
 internal data class ExternalPayerInfoRequest(
     val email: String?,
-    val externalPayerId: String?
+    val externalPayerId: String?,
+    val firstName: String?,
+    val lastName: String?
 ) : JSONObjectSerializable {
     companion object {
 
         private const val EMAIL_FIELD = "email"
-        private const val EXTERNAL_PAYER_ID = "external_payer_id" // BE does not support camelCase
+        private const val EXTERNAL_PAYER_ID_FIELD = "external_payer_id" // BE does not support camelCase
+        private const val FIRST_NAME_FIELD = "first_name" // BE does not support camelCase
+        private const val LAST_NAME_FIELD = "last_name" // BE does not support camelCase
 
         @JvmField
         val serializer = JSONObjectSerializer<ExternalPayerInfoRequest> { t ->
             JSONObject().apply {
                 put(EMAIL_FIELD, t.email)
-                put(EXTERNAL_PAYER_ID, t.externalPayerId)
+                put(EXTERNAL_PAYER_ID_FIELD, t.externalPayerId)
+                put(FIRST_NAME_FIELD, t.firstName)
+                put(LAST_NAME_FIELD, t.lastName)
             }
         }
     }
